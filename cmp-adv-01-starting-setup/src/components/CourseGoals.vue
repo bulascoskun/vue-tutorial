7964cb3b1948385cc04e3748440992d41d8e6b43
bulascoskun <ulas_coskun99@hotmail.com>
2024-04-30 00:11:36 +0300

    completed section 9

---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" :another-prop="'lorem ipsum'"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['Finish the course', 'Learn vue'],
+    };
+  },
+};
+</script>
