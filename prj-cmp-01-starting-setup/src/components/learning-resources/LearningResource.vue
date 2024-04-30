5661afb5bb4cab91c4ca2eedca72aa6dbfc2c18f
bulascoskun <ulas_coskun99@hotmail.com>
2024-04-30 16:00:10 +0300

    initialized prj

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav></nav>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
