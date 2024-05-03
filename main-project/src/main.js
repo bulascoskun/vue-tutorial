33c5d283c50ef2b4f47d8ff4e0baf82aee5aaa22
bulascoskun <ulas_coskun99@hotmail.com>
2024-05-04 01:23:51 +0300

    completed section 16

--- main-project/src/main.js
+++ main-project/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 
-createApp(App).mount('#app');
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import BaseCard from './pages/ui/BaseCard.vue';
+import BaseButton from './pages/ui/BaseButton.vue';
+import BaseBadge from './pages/ui/BaseBadge.vue';
+import BaseSpinner from './pages/ui/BaseSpinner.vue';
+import BaseDialog from './pages/ui/BaseDialog.vue';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
+
+app.mount('#app');
---
+++ main-project/src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not found.</p>
+      <h4>Return <router-link to="coaches">home</router-link></h4>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
